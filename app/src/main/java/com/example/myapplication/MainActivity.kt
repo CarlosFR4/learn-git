9f1e76fdd47d379407590e712f39fd07f118a340
CarlosFR4 <cfr4123@gmail.com>
2024-07-14 10:23:12 -0600

    The other file

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -30,14 +30,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
@@ -45,5 +37,3 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
-
-/// In branch copy
