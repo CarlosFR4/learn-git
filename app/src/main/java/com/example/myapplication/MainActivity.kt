bd25fe7bc91c430348b53828c4948ebaac989bf4
CarlosFR4 <cfr4123@gmail.com>
2024-07-14 11:42:20 -0600

    comentario en main

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,6 +15,7 @@ import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // comentario en main
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
